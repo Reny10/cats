ad09c3dfb31da60d04cf5c2759b7cc1b3cfbd8dc
Reny10 <60260308+Reny10@users.noreply.github.com>
2020-06-02 03:28:00 +0100

    Add files via upload

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,8 @@ const SearchBox = ({ searchfield, searchChange }) => {
 console.log('SearchBox')
   return (
     <div className='pa2'>
-   <input className='pa3 ba b--green bg-lightest-blue' type='search'
+   <input aria-label='Search cats'
+          className='pa3 ba b--green bg-lightest-blue' type='search'
           placeholder='Search cats'onChange={searchChange}/>
     </div>
   );
