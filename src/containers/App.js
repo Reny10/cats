5c97a8c187800232dfe14f74477de1bdb96e679c
Reny10 <60260308+Reny10@users.noreply.github.com>
2020-06-06 22:40:40 +0100

    Add files via upload

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,8 +16,7 @@ const mapStateToProps = (state) => {
     cats: state.requestCats.cats,
     isPending: state.requestCats.isPending,
     error: state.requestCats.error
-    }
-    }
+    }}
 
 // dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
 // the function returns an object then uses connect to change the data from redecers.
@@ -25,38 +24,20 @@ const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
     onRequestCats: () => dispatch(requestCats())
-  }
-}
+  }}
 
 class App extends Component {
-
 componentDidMount() {
     this.props.onRequestCats();
   }
-
-   /*constructor(){
-    super()
-    this.state={
-  cats:cats
-  //searchfield:''
-  }}
-   /*componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ cats: users})});
-  }*/
-    //onSearchChange = (event) => {
-    //this.setState({ searchfield: event.target.value })}
-
-  render() {
+   render() {
     const { searchField, onSearchChange, cats, isPending } = this.props;
     const filteredCats = cats.filter(cat =>{
-      return cat.name.toLowerCase().includes(searchField.toLowerCase());
+    return cat.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return isPending?
      <h1>Loading</h1>:
-    (
-    <div className='tc'>
+    ( <div className='tc'>
     <Header/>
     <SearchBox searchChange={onSearchChange}/>
     <Scroll>
